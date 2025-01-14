04ed5e329f65107c30c5897b088cded9d0dea076
Beatstamosi <moritz.lued@gmail.com>
2025-01-14 15:24:55 -0500

    Add & Style pop up form to add new task or project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./styles.css";
+
+// pop up form to add task
+const dialogTask = document.querySelector("#dialog-add-task");
+const addTaskButton = document.querySelector("#add-task");
+
+addTaskButton.addEventListener("click", () => {
+    dialogTask.showModal();
+})
+
+// pop up form to add project
+const dialogProject = document.querySelector("#dialog-add-project");
+const addProjectButton = document.querySelector("#add-project");
+
+addProjectButton.addEventListener("click", () => {
+    dialogProject.showModal();
+})
